7e8d19913fbe90824387b91c8c63611ec50acbd4
JKEOSHKE3745 <JKEOSHKE3745@W10-9R51NL3>
2023-02-21 09:13:56 -0600

    Add project files.

---
+++ Discount Project/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstDiscounts = New System.Windows.Forms.ListBox()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblSaved = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstDiscounts
+        '
+        Me.lstDiscounts.FormattingEnabled = True
+        Me.lstDiscounts.Items.AddRange(New Object() {"10%", "15%", "20%", "25%", "30%", "35%", "40%"})
+        Me.lstDiscounts.Location = New System.Drawing.Point(123, 51)
+        Me.lstDiscounts.Name = "lstDiscounts"
+        Me.lstDiscounts.Size = New System.Drawing.Size(34, 95)
+        Me.lstDiscounts.TabIndex = 0
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(28, 51)
+        Me.txtPrice.Multiline = True
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(60, 33)
+        Me.txtPrice.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Original Price"
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(28, 112)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(63, 34)
+        Me.btnCalc.TabIndex = 3
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(284, 182)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(115, 35)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Discount"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 172)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Amount Saved"
+        '
+        'lblSaved
+        '
+        Me.lblSaved.BackColor = System.Drawing.Color.White
+        Me.lblSaved.Location = New System.Drawing.Point(31, 191)
+        Me.lblSaved.Name = "lblSaved"
+        Me.lblSaved.Size = New System.Drawing.Size(63, 22)
+        Me.lblSaved.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(123, 171)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Total Price"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.Color.White
+        Me.lblTotal.Location = New System.Drawing.Point(120, 191)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(63, 22)
+        Me.lblTotal.TabIndex = 9
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(371, 217)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.lblSaved)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.lstDiscounts)
+        Me.Name = "frmMain"
+        Me.Text = "Main Form.vb"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstDiscounts As ListBox
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblSaved As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblTotal As Label
+End Class
---
+++ Discount Project/Form1.vb
@@ -0,0 +1,40 @@
+﻿Option Infer Off
+Option Strict On
+Option Explicit On
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        Dim x As Double
+        Dim Total As Double
+
+        If lstDiscounts.SelectedIndex = 0 Then
+            x = 0.1
+        ElseIf lstDiscounts.SelectedIndex = 1 Then
+            x = 0.15
+        ElseIf lstDiscounts.SelectedIndex = 2 Then
+            x = 0.2
+        ElseIf lstDiscounts.SelectedIndex = 3 Then
+            x = 0.25
+        ElseIf lstDiscounts.SelectedIndex = 4 Then
+            x = 0.3
+        ElseIf lstDiscounts.SelectedIndex = 5 Then
+            x = 0.35
+        ElseIf lstDiscounts.SelectedIndex = 6 Then
+            x = 0.4
+        End If
+
+        Double.TryParse(txtPrice.Text, Total)
+        lblTotal.Text = (Total - (Total * x)).ToString("C2")
+
+        lblSaved.Text = (Total * x).ToString("C2")
+    End Sub
+
+    Private Sub lstDiscounts_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstDiscounts.SelectedIndexChanged, txtPrice.TextChanged
+        lblSaved.Text = String.Empty
+        lblTotal.Text = String.Empty
+    End Sub
+End Class
